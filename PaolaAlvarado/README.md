6c0c69c35df1bff0ec6958488ccf3e7391c6dbc9
Andrea_Nina <alvaradoandrea456@gmail.com>
2022-08-02 16:53:23 -0400

    Added proyect and readme file

---
+++ PaolaAlvarado/README.md
@@ -0,0 +1,6 @@
+VERIFICADOR DE TRIANGULOS
+
+Abrir la carpeta TipodeTriangulo
+Click en TipodeTriangulo.sln
+Una vez abierto el programa, presionar en el botón TipodeTriangulo en la parte superior del programa.
+LLenar los datos indicados en la consola desplegada
---
+++ PaolaAlvarado/TipodeTriangulo/TipodeTriangulo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TipodeTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verificador de triangulo
+
+            Console.WriteLine("Ingrese el lado a del triangulo");
+            bool isA = double.TryParse(Console.ReadLine(),out double a);
+
+            Console.WriteLine("Ingrese el lado b del triangulo");
+            bool isB= double.TryParse(Console.ReadLine(),out double b);
+
+            Console.WriteLine("Ingrese el lado c del triangulo");
+            bool isC =double.TryParse(Console.ReadLine(),out double c);
+
+            if(isA && isB && isC)
+            {
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Triangulo equilatero");
+                }
+                else if (c == a || c == b || a == b)
+                {
+                    Console.WriteLine("Triangulo isosceles");
+                }
+                else
+                {
+                    Console.WriteLine("Triangulo escaleno");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Los lados del triangulo deben ser valores numericos");
+            }
+            
+        }
+    }
+}
