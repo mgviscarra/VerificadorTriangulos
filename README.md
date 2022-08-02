87e500cf1669c13ead4ca1996b6e161511153f8b
GaliaGarnica <ga.gga4747@gmail.com>
2022-08-02 17:50:34 -0400

    Verificador T was created

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # VerificadorTriangulos
+Para poder ejecutar el programa :
+1. Ingrese a VerificadorT > VerificadorT > bin > Debug > net5.0 
+2. Por ultimo ingrese al ejecutable VerificadorT.exe
+2. Descargue el ejecutable haciendo click en View raw
+3. Busque en su carpeta "Descargas" el ejecutable y haga click sobre este para iniciar 
+
---
+++ VerificadorT/VerificadorT/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace VerificadorT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            tipotriangulo();
+        }
+        public static void tipotriangulo() 
+        {
+            try
+            {
+                double a = 0, b = 0, c = 0;
+
+                Console.WriteLine("Ingrese lado a ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese lado b ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese lado c ");
+                c = Convert.ToDouble(Console.ReadLine());
+
+                if (a <= 0 || b <= 0 || c <= 0)
+                {
+                    Console.WriteLine("Todos los valores ingresados deben ser mayores a 0");
+                    tipotriangulo();
+                }
+                else { 
+                    if (a == b && b == c)
+                        Console.WriteLine("Triangulo equilatero");
+                    else if (a == b || b == c || a == c)
+                        Console.WriteLine("Triangulo isoceles ");
+                    else
+                        Console.WriteLine("Triangulo escaleno ");
+
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Intente nuevamente, solo se admiten números");
+                tipotriangulo();
+            }
+        }
+    }
+}
