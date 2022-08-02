79a2c88dec01d78f1382ab38fcf3ba5225fa9691
Gaboteg <gabotellezg@gmail.com>
2022-08-02 14:38:06 -0700

    programa y actualizacion de readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # VerificadorTriangulos
+Para ejecutar el programa abra el archivo .sln usando visual studio y haga clic en el boton "iniciar" (es una aplicacion de consola)
---
+++ Verificador/Verificador/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verificador
+{
+    public class Exercise14
+    {
+        public static void Main()
+        {
+            int lado1, lado2, lado3;
+            string l1, l2, l3;
+            Console.Write("\n\n");
+            Console.Write("verificar si un triangulo es equilatero, isoceles o escaleno:\n");
+            Console.Write("===============================================");
+            Console.Write("\n\n");
+
+
+
+            Console.Write("Lado 1 del triangulo ");
+            lado1 = Convert.ToInt32(Console.ReadLine());
+            
+
+
+            Console.Write("Lado 2 del triangulo ");
+            lado2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Lado 3 del triangulo ");
+            lado3 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.Write("Triangulo equilatero.\n");
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.Write("Triangulo isoceles.\n");
+            }
+            else
+            {
+                Console.Write("Triangulo escaleno.\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
